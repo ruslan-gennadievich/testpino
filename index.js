3a06c7b4a2d4b6ffb634d66e8694eb9a9ee10b45
Ruslan Bychikhin <vb.xakep@gmail.com>
2020-06-16 22:55:13 +0300

    redefine console to logger

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const logger = require('pino')()
+console = logger
+console.log = (data) => logger.info(data)
+
 
 let i = 0;
-setInterval ( () => {  
-  logger.info('hello world #' + i)
+setInterval ( () => {
+  console.log('hello world #' + i)
   i++;
 }, 1000)
